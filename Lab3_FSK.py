05528395de180061b6a3127aa59693a7ec5e1f27
Ryan Kearns <Ryan.Kearns.du.edu>
2023-10-12 21:31:28 -0600

    Trying math equations

--- Lab3_FSK.py
+++ Lab3_FSK.py
@@ -99,43 +99,8 @@ class Lab3_FSK(gr.top_block, Qt.QWidget):
         self._threshold_line_edit.returnPressed.connect(
             lambda: self.set_threshold(eng_notation.str_to_num(str(self._threshold_line_edit.text()))))
         self.top_layout.addWidget(self._threshold_tool_bar)
-        self.qtgui_waterfall_sink_x_0_0 = qtgui.waterfall_sink_c(
-            1024, #size
-            window.WIN_BLACKMAN_hARRIS, #wintype
-            freq, #fc
-            samp_rate, #bw
-            "Demodulator Input", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_waterfall_sink_x_0_0.set_update_time(0.10)
-        self.qtgui_waterfall_sink_x_0_0.enable_grid(False)
-        self.qtgui_waterfall_sink_x_0_0.enable_axis_labels(True)
-
-
-
-        labels = ['', '', '', '', '',
-                  '', '', '', '', '']
-        colors = [0, 0, 0, 0, 0,
-                  0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-                  1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_waterfall_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_waterfall_sink_x_0_0.set_line_label(i, labels[i])
-            self.qtgui_waterfall_sink_x_0_0.set_color_map(i, colors[i])
-            self.qtgui_waterfall_sink_x_0_0.set_line_alpha(i, alphas[i])
-
-        self.qtgui_waterfall_sink_x_0_0.set_intensity_range(-140, 10)
-
-        self._qtgui_waterfall_sink_x_0_0_win = sip.wrapinstance(self.qtgui_waterfall_sink_x_0_0.qwidget(), Qt.QWidget)
-
-        self.top_layout.addWidget(self._qtgui_waterfall_sink_x_0_0_win)
         self.qtgui_waterfall_sink_x_0 = qtgui.waterfall_sink_c(
-            1024, #size
+            32768, #size
             window.WIN_BLACKMAN_hARRIS, #wintype
             0, #fc
             samp_rate, #bw
@@ -217,71 +182,67 @@ class Lab3_FSK(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
             1024, #size
-            samp_rate, #samp_rate
-            "Output Vector", #name
-            1, #number of inputs
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            1,
             None # parent
         )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
-
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis((-140), 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
 
 
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
-            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
             1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [-1, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
 
         for i in range(1):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(1, firdes.low_pass(1,samp_rate,filter_cutoff,filter_transition), freq, samp_rate)
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_ccc(20, firdes.low_pass(1,samp_rate,filter_cutoff,filter_transition), freq, samp_rate)
         self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
         self.blocks_vector_source_x_0 = blocks.vector_source_f((-1, 1, 1, 1, -1, 1, -1, -1, 1, 1, -1, -1, -1, 1, 1,-1,1,1,-1,-1,-1,1,-1,-1,1,1,1), True, 1, [])
         self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
         self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, 600)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
         self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
         self.blocks_add_const_vxx_0 = blocks.add_const_ff((-.5))
         self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, freq, 1, 0, 0)
         self.analog_quadrature_demod_cf_0 = analog.quadrature_demod_cf((samp_rate/(2*math.pi*fsk_deviation_hz)))
-        self.analog_frequency_modulator_fc_0 = analog.frequency_modulator_fc(((math.pi*fsk_deviation_hz)/samp_rate))
+        self.analog_frequency_modulator_fc_0 = analog.frequency_modulator_fc(((2*math.pi*fsk_deviation_hz)/samp_rate))
 
 
         ##################################################
         # Connections
         ##################################################
         self.connect((self.analog_frequency_modulator_fc_0, 0), (self.blocks_multiply_xx_0, 0))
+        self.connect((self.analog_frequency_modulator_fc_0, 0), (self.qtgui_freq_sink_x_0, 0))
         self.connect((self.analog_quadrature_demod_cf_0, 0), (self.blocks_add_const_vxx_0, 0))
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_add_const_vxx_0, 0), (self.digital_binary_slicer_fb_0, 0))
@@ -290,11 +251,10 @@ class Lab3_FSK(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_repeat_0, 0), (self.analog_frequency_modulator_fc_0, 0))
         self.connect((self.blocks_repeat_0, 0), (self.qtgui_time_sink_x_1, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.blocks_multiply_xx_0, 1))
-        self.connect((self.blocks_uchar_to_float_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.blocks_null_sink_0, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_repeat_0, 0))
         self.connect((self.digital_binary_slicer_fb_0, 0), (self.blocks_uchar_to_float_0, 0))
         self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.analog_quadrature_demod_cf_0, 0))
-        self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.qtgui_waterfall_sink_x_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -323,15 +283,14 @@ class Lab3_FSK(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.analog_frequency_modulator_fc_0.set_sensitivity(((math.pi*self.fsk_deviation_hz)/self.samp_rate))
+        self.analog_frequency_modulator_fc_0.set_sensitivity(((2*math.pi*self.fsk_deviation_hz)/self.samp_rate))
         self.analog_quadrature_demod_cf_0.set_gain((self.samp_rate/(2*math.pi*self.fsk_deviation_hz)))
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
         self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(1,self.samp_rate,self.filter_cutoff,self.filter_transition))
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate)
         self.qtgui_waterfall_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.qtgui_waterfall_sink_x_0_0.set_frequency_range(self.freq, self.samp_rate)
 
     def get_gain(self):
         return self.gain
@@ -344,7 +303,7 @@ class Lab3_FSK(gr.top_block, Qt.QWidget):
 
     def set_fsk_deviation_hz(self, fsk_deviation_hz):
         self.fsk_deviation_hz = fsk_deviation_hz
-        self.analog_frequency_modulator_fc_0.set_sensitivity(((math.pi*self.fsk_deviation_hz)/self.samp_rate))
+        self.analog_frequency_modulator_fc_0.set_sensitivity(((2*math.pi*self.fsk_deviation_hz)/self.samp_rate))
         self.analog_quadrature_demod_cf_0.set_gain((self.samp_rate/(2*math.pi*self.fsk_deviation_hz)))
 
     def get_freq(self):
@@ -355,7 +314,6 @@ class Lab3_FSK(gr.top_block, Qt.QWidget):
         Qt.QMetaObject.invokeMethod(self._freq_line_edit, "setText", Qt.Q_ARG("QString", eng_notation.num_to_str(self.freq)))
         self.analog_sig_source_x_0.set_frequency(self.freq)
         self.freq_xlating_fir_filter_xxx_0.set_center_freq(self.freq)
-        self.qtgui_waterfall_sink_x_0_0.set_frequency_range(self.freq, self.samp_rate)
 
     def get_filter_transition(self):
         return self.filter_transition
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Lab 3
 
 ## Introduction
-Lab 3's objective is to build an end-to-end FSK Simulation. It includes both the modulation and demodulation portions of the FSK path.
+Lab 3's objective is to build an end-to-end FSK Simulation. It includes both the modulation and demodulation portions of the FSK path. FSK is the encoding of binary information into the frequency deomain information. Most commonly, specific frequency values will translate to different distinct frequencies.
 
 ## Equipment List
 The following section outlines the equipment used in this lab. All equipment can be purchased following the links provided
@@ -32,3 +32,18 @@ The below figure shows the overall flow diagram of the algorithm. It includes th
 The below section shows the modulation from vector source through to frequency mixing. The key sections of this simulation is the Vector Source, repeater, frequency modulator, and mixer. 
 
 ![alt text](https://github.com/Ryankearns9/DigComm_Lab3/blob/main/img/Rx_Flow.PNG)
+
+#### Vector Source
+![alt text](https://github.com/Ryankearns9/DigComm_Lab3/blob/main/img/VectorSource.PNG)
+
+The vector source is the symbol list used for the FSK signal. This is a randomly generated list of symbols alternating between -1 and +1 where -1 represents a binary 0 and +1 represents a binary 1. This list is repeated to allow for the simulation of an arbritrarily long data vector.
+
+#### Repeat Block
+![alt text](https://github.com/Ryankearns9/DigComm_Lab3/blob/main/img/Repeat.PNG)
+
+The repeat block will replicate an input N times. This creates an oversampling ratio of N such that there are N samples per symbol. In this case, the oversampling ratio was chosen to be 600. This ratio was chosen to allow for a clean looking time-domain plot.
+
+#### Frequency Modulation
+![alt text](https://github.com/Ryankearns9/DigComm_Lab3/blob/main/img/FrequencyMod.PNG)
+
+The frequency modulation translates changes in input amplitude to changes in frequency. The equation which describes this translation is shown below $e^(2*\pi)$
